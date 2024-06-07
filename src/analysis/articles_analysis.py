5573f8057bc232adee7ddf8b1f8b1edf7ba367b5
Tony <tonymeissner70@gmail.com>
2024-06-07 15:41:16 +0900

    refactor: Update article file path in articles_analysis.py

--- src/analysis/articles_analysis.py
+++ src/analysis/articles_analysis.py
@@ -5,20 +5,17 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from config import RAW_DATA_PATH, PROCESSED_DATA_PATH, CELEBRITIES
 
+
 if __name__ == "__main__":
     for celebrity in CELEBRITIES:
         name = celebrity["name"]
-        file_path = os.path.join(RAW_DATA_PATH, f"{name}_articles.csv")
+        file_path = os.path.join(RAW_DATA_PATH, f"{name}_articles_gnews.csv")
         articles_df = pd.read_csv(file_path)
 
         print(f"Processing {name}...")
         print(f"Total articles: {len(articles_df)}")
 
-        # TODO
-        senti_scores_df = articles_df.copy()
+        articles_df["published_on"] = pd.to_datetime(articles_df["published_on"])
+        articles_df = articles_df.sort_values(by="published_on")
 
-        # Save sentiment scores
-        output_file_path = os.path.join(
-            PROCESSED_DATA_PATH, f"{name}_articles_senti.csv"
-        )
-        senti_scores_df.to_csv(output_file_path, index=False)
+        print(articles_df.info())
