771aeeea6c2cf982a3cb4a31354c4a0adee839fe
Tony <tonymeissner70@gmail.com>
2024-06-07 23:03:48 +0900

    refactor: Update celebrities dataframe preparation
    Refactor the code to update the preparation of the celebrities dataframe. This includes converting names and search terms to lowercase, converting date columns to UTC timezone, and updating the dataframe itself.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 from datetime import datetime, time
 import os
 from helpers.file_functions import load_json
+import pandas as pd
 
 # Load environment variables
 from dotenv import load_dotenv
@@ -12,10 +13,30 @@ GNEWS_API_KEY = os.getenv("GNEWS_API_KEY")
 SERP_API_KEY = os.getenv("SERP_API_KEY")
 
 # File Paths
-CWD = os.getcwd()
+CWD = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RAW_DATA_PATH = os.path.join(CWD, "data", "raw")
 PROCESSED_DATA_PATH = os.path.join(CWD, "data", "processed")
 
 # Celebrities
 config_path = os.path.join(CWD, "config.json")
-CELEBRITIES = load_json(config_path)
+CELEBRITIES = pd.DataFrame(load_json(config_path))
+
+
+def prepare_celebrities(df):
+    """
+    Prepare the celebrities dataframe
+    """
+    df["name"] = df["name"].str.lower()
+    df["search_term"] = df["search_term"].str.lower()
+
+    df["start_date"] = pd.to_datetime(df["start_date"]).dt.tz_convert("UTC")
+    df["end_date"] = pd.to_datetime(df["end_date"]).dt.tz_convert("UTC")
+    df["cancellation_date"] = pd.to_datetime(df["cancellation_date"]).dt.tz_convert(
+        "UTC"
+    )
+
+    return df
+
+
+# Prepare the CELEBRITIES dataframe
+CELEBRITIES = prepare_celebrities(CELEBRITIES)
